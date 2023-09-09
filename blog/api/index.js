fcbcb49c23cdffdf070d57008889e3f71eb2a191
shubham <tonygupta275@gmail.com>
2023-09-09 21:45:30 +0530

    working on login page set cookie and token

--- blog/api/index.js
+++ blog/api/index.js
@@ -1,28 +1,64 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-// const User=require('./models/User')
 import UserModel from './models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+const salt = bcrypt.genSaltSync(10); //bycryptjs
+const secret = 'rgrffbfbfbfb232832cy28h7327t574233424cg@#ege#'; //jwt secret key
 
 const MONGO_URL = `mongodb+srv://tonygupta275:zoAcRqDwTeldGSC2@cluster0.6efrmtj.mongodb.net/blog?retryWrites=true&w=majority`;
 mongoose.connect(MONGO_URL);
 
-const app = express();
 
-app.use(cors());
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
     try {
-        const userDoc = await UserModel.create({ username, password });
+        const userDoc = await UserModel.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
         res.json(userDoc);
 
     } catch (error) {
         res.status(400).send(error);
     }
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await UserModel.findOne({ username });
+    const passOK = bcrypt.compareSync(password, userDoc.password);
+
+    if (passOK) {
+        //logged in
+        jwt.sign({ username, id:userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token',token).json('ok');
+        })
+    }
+    else {
+        res.status(400).send("wrong credentional");
+    }
+})
+
+app.get('/profile',(req,res)=>{
+    const {token} =req.cookies;
+    jwt.verify(token,secret,{},(err,info)=>{
+        if(err) throw err;
+        res.json(info);
+    });
+    res.json(req.cookies);
 })
 
 app.listen("4000", () => {
--- blog/blog/src/pages/LoginPage.js
+++ blog/blog/src/pages/LoginPage.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Navigate} from 'react-router-dom';
 
 const LoginPage = () => {
+  
+  const [username, setusername] = useState("");
+  const [password, setpassword] = useState("");
+  const [redirect, setredirect] = useState(false);
+
+  const login=async(e)=>{
+    e.preventDefault();
+    const response= await fetch('http://localhost:4000/login',{
+      method:'POST',
+      body:JSON.stringify({username,password}),
+      headers:{'Content-Type':'application/json'},
+      credentials:'include',
+    });
+    if(response.ok){
+      setredirect(true);
+    }else
+    {
+      alert("wrong credentials");
+    }
+  }
+
+  if(redirect){
+    return  <Navigate to={'/'}/>
+  }
   return (
     <>
-      <form className='login'>
+      <form className='login' onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
+        <input
+          type="text"
+          placeholder='username'
+          value={username}
+          onChange={e => setusername(e.target.value)} />
+        <input
+          type="password"
+          placeholder='password'
+          value={password}
+          onChange={e => setpassword(e.target.value)} />
+
         <button>Login</button>
       </form>
     </>
