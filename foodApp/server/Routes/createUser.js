d77cb4a1f0df4e9b447ac30e5a0e96da3beb47fe
shubham <tonygupta275@gmail.com>
2023-09-25 21:13:27 +0530

    make expres routes

---
+++ foodApp/server/Routes/createUser.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import UserModel from '../models/User.js';
+
+const router =express.Router();
+
+
+ const createuser=router.post('/createuser',async(req,res)=>{
+    try {
+        UserModel.create({
+            name:req.body.name,
+            location:req.body.location,
+            email:req.body.email,
+            password:req.body.password,
+        })
+        res.json({success:true});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false});
+    }
+
+});
+
+export default createuser;
