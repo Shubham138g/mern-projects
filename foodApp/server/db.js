98459eafe367c9798130be30ef60156c56bdc1b6
shubham <tonygupta275@gmail.com>
2023-09-25 21:11:22 +0530

    make databae conn

--- foodApp/server/db.js
+++ foodApp/server/db.js
@@ -1,14 +1,17 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const MONGOURL=`mongodb+srv://tonygupta275:SJ0huKQz7xbe2e6K@loginsystem.5di64ac.mongodb.net/?retryWrites=true&w=majority`;
+const MONGOURL = `mongodb+srv://tonygupta275:SJ0huKQz7xbe2e6K@loginsystem.5di64ac.mongodb.net/foodorder?retryWrites=true&w=majority`;
 
-const conn=()=>{
+const conn = async () => {
     try {
-        mongoose.connect(MONGOURL)
+        await mongoose.connect(MONGOURL)
         console.log('db connected');
-        
+        const fetch_data = await mongoose.connection.db.collection("food_items");
+      const data=  await fetch_data.find({}).toArray()
+      console.log();
+
     } catch (error) {
-        console.log('db not conneted',error);
+        console.log('db not conneted', error);
     }
 
 }
