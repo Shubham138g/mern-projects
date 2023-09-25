8b64582452c0a89d8db66747b5dd676af2a609fb
shubham <tonygupta275@gmail.com>
2023-09-25 21:11:48 +0530

    make express server

--- foodApp/server/index.js
+++ foodApp/server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import conn from '../server/db.js';
+import createuser from '../server/Routes/createUser.js'
 
 const app=express();
 const port=4000;
 conn();
 
+app.use(express.json());
 
+
+app.use('/api',createuser )
 
 app.get("/",(req,res)=>{
     res.send('hello world');
