0c6761c46bebeff8fda72d449ad64b21e6cfd8ee
shubham <tonygupta275@gmail.com>
2023-09-27 21:46:03 +0530

    changes in app.js

--- foodApp/client/src/App.js
+++ foodApp/client/src/App.js
@@ -5,6 +5,7 @@ import Login from './Screens/Login';
 import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Signup from './Screens/Signup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/createuser' element={<Signup />} />
         </Routes>
 
       </BrowserRouter>
