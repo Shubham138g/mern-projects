2bc62766d3d675283449deef715f3a3fb13d3ad0
shubham <tonygupta275@gmail.com>
2023-09-27 21:47:16 +0530

    make signup page

--- foodApp/server/Routes/createUser.js
+++ foodApp/server/Routes/createUser.js
@@ -1,26 +1,38 @@
 import express from 'express';
 import UserModel from '../models/User.js';
+import { body, validationResult } from 'express-validator';
 
-const router =express.Router();
-
-
-
+const router = express.Router();
 
 
- const createuser=router.post('/createuser',async(req,res)=>{
-    try {
-        UserModel.create({
-            name:req.body.name,
-            location:req.body.location,
-            email:req.body.email,
-            password:req.body.password,
-        })
-        res.json({success:true});
-    } catch (error) {
-        console.log(error);
-        res.json({success:false});
-    }
 
-});
+
+
+const createuser = router.post('/createuser',
+    [
+        body('email').isEmail(),
+        body('password').isLength({ min: 4 }),
+        body('name').isLength({ min: 4 }),
+        body('location').isLength({ min: 4 }),
+    ],
+    async (req, res) => {
+        const errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        try {
+            UserModel.create({
+                name: req.body.name,
+                location: req.body.location,
+                email: req.body.email,
+                password: req.body.password,
+            })
+            res.json({ success: true });
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false });
+        }
+
+    });
 
 export default createuser;
