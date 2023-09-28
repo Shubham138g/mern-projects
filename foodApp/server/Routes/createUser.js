32d8de2cc74756b7f873810abae2cab0eb5c8c16
shubham <tonygupta275@gmail.com>
2023-09-28 21:30:35 +0530

    work on login routes

--- foodApp/server/Routes/createUser.js
+++ foodApp/server/Routes/createUser.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import UserModel from '../models/User.js';
 import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
+const jwtSecret="mynameisshubhamguptaandiamastudent";
 
-
-
-
-
-const createuser = router.post('/createuser',
+ router.post('/createuser',
     [
         body('email').isEmail(),
         body('password').isLength({ min: 4 }),
@@ -20,12 +19,16 @@ const createuser = router.post('/createuser',
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()});
         }
+
+        const salt= await bcrypt.genSalt(10);
+        let secPassword= await bcrypt.hash(req.body.password,salt);
+
         try {
             UserModel.create({
                 name: req.body.name,
                 location: req.body.location,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPassword,
             })
             res.json({ success: true });
         } catch (error) {
@@ -34,5 +37,40 @@ const createuser = router.post('/createuser',
         }
 
     });
+ router.post('/loginuser',
+ [
+    body('email').isEmail(),
+    body('password').isLength({ min: 4 }),
+    body('name').isLength({ min: 4 }),
+    body('location').isLength({ min: 4 }),
+], async (req, res) => {
+        const email=req.body.email;
+        try {
+            const userData=await UserModel.findOne({email});
+            if(!userData){
+                return res.status(400).json({errors:"try loging with correct credentials"});
+            }
 
-export default createuser;
+            const passwordCompare= await bcrypt.compare(req.body.password,userData.password)
+
+            if(!passwordCompare){
+                return res.status(400).json({errors:"try loging with correct credentials"});
+            }
+            const data={
+                user:{
+                    id:userData.id
+                }
+            }
+            const authtoken=jwt.sign(data,jwtSecret);
+                return res.json({success:true,authtoken:authtoken})
+           
+        } catch (error) {
+            console.log(error);
+            res.json({ success: false });
+        }
+
+    });
+
+    
+
+export default router;
