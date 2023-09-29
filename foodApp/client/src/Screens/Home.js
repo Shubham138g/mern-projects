25b42cb187ac46e664e6b87f8ebbff055cff2b07
shubham <tonygupta275@gmail.com>
2023-09-29 14:08:52 +0530

    used map function in home

--- foodApp/client/src/Screens/Home.js
+++ foodApp/client/src/Screens/Home.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../Components/NavBar';
 import Footer from '../Components/Footer';
 import Card from '../Components/Card';
 import Carousel from '../Components/Carousel';
 
 const Home = () => {
+
+    const [foodCat, setfoodCat] = useState([]);
+    const [foodItem, setfoodItem] = useState([]);
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:4000/api/foodData", {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+
+        response = await response.json();
+        setfoodItem(response[0]);
+        setfoodCat(response[1]);
+        // console.log(response[0], response[1]);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
+
+
+
+
+
+
     return (
         <>
-            <NavBar />
-            <div>
-                <Carousel />
-            </div>
-            <div className='m-3'>
+            <div><NavBar /> </div>
+            <div><Carousel /></div>
+            <div className='container'>
+                {
+                    foodCat !== []
+                        ? foodCat.map((data) => {
+                            return (
+                                <div>hello world</div>
+                            )
+                        })
+                        :
+                        <div>''''''''''''''''''''''''''''</div>
+                }
                 <Card />
             </div>
-            <Footer />
+            <div><Footer /></div>
 
         </>
     );
