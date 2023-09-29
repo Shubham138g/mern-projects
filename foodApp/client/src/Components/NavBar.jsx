fa502df06e0878a7da8c9709e92c3d68232d26c8
shubham <tonygupta275@gmail.com>
2023-09-29 20:30:34 +0530

    use ternary operator in navbar

--- foodApp/client/src/Components/NavBar.jsx
+++ foodApp/client/src/Components/NavBar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate=useNavigate();
+  const handleLogout=()=>{
+    localStorage.removeItem("authtoken");
+    navigate("/login")
+  }
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
@@ -23,21 +28,39 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
+                <Link className="nav-link active fs-5 " aria-current="page" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/createuser">
-                  Signup
-                </Link>
-              </li>
+              {
+                (localStorage.getItem("authtoken"))
+                ?
+                 <li className="nav-item">
+                 <Link className="nav-link active fs-5 " aria-current="page" to="/">
+                   My Order
+                 </Link>
+               </li>
+               :""
+              }
             </ul>
+            {
+              (!localStorage.getItem("authtoken"))
+              ?
+            <div className="d-flex">
+              <Link className="btn bg-white text-black mx-1" to="/login">  Login</Link>
+              <Link className="btn bg-white text-black mx-1" to="/createuser">  Signup </Link>
+            </div>
+            :
+            <>
+             <div className="btn bg-white text-black mx-2">
+              My Cart
+            </div>
+            <div className="btn bg-white text-danger  mx-2" onClick={handleLogout}>
+              logout
+            </div>
+            </>
+           
+}
           </div>
         </div>
       </nav>
