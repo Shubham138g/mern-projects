a2d8b219fcef88e7f9f6c6316041d182478d8dbe
shubham <tonygupta275@gmail.com>
2023-09-29 20:30:00 +0530

    display cards

--- foodApp/client/src/Components/Card.jsx
+++ foodApp/client/src/Components/Card.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  let options = props.options;
+  let priceoptions = Object.keys(options);
+
+  const handleAddCart=()=>{
+    
+  }
   return (
     <>
       <div>
         <div class="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-          <img src="https://imgs.search.brave.com/fq5XQP7deYTygo4Hc4ABXWgiAdgR-YMk9RFVFTh56s8/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vcGljanVt/Ym8uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy9mcmllcy1mb29k/LWZyZWUtcGhvdG8u/anBnP3c9NjAwJnF1/YWxpdHk9ODA" class="card-img-top" alt="..." />
+         
+          <img src={props.imgSrc} class="card-img-top" alt="..." style={{height:"190px",objectFit:"fill"}} />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the.</p>
+            <h5 class="card-title">{props.foodName}</h5>
+
             <div className="container w-100">
               <select className="m-2  h-100 bg-primary">
                 {Array.from(Array(6), (e, i) => {
@@ -21,11 +28,18 @@ const Card = () => {
                 })}
               </select>
               <select className="m-2  h-100 bg-primary">
-                <option value="half">Half</option>
-                <option value="Full">Full</option>
+                {priceoptions.map((data) => {
+                  return (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  );
+                })}
               </select>
               <div className="d-inline fs-5">Total Price</div>
             </div>
+            <hr />
+            <div className="btn btn-primary justify-center ms-2" onClick={handleAddCart}>Add to cart</div>
           </div>
         </div>
       </div>
