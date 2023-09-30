c830df8a6257f7b853604584aae8ebb8b4d8fb58
shubham <tonygupta275@gmail.com>
2023-09-30 20:47:27 +0530

    modified   foodApp/client/src/App.js

--- foodApp/client/src/App.js
+++ foodApp/client/src/App.js
@@ -6,18 +6,21 @@ import "../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Signup from './Screens/Signup';
+import { CardProvider } from './Components/ContextReducer';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/createuser' element={<Signup />} />
-        </Routes>
+      <CardProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/createuser' element={<Signup />} />
+          </Routes>
 
-      </BrowserRouter>
+        </BrowserRouter>
+      </CardProvider>
 
     </>
   );
