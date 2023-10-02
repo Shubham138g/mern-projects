94b1ee151b249fc04161b8ced2c9cfa666e2d024
shubham <tonygupta275@gmail.com>
2023-10-02 19:23:15 +0530

    modify

--- foodApp/client/src/Screens/MyOrder.js
+++ foodApp/client/src/Screens/MyOrder.js
@@ -47,6 +47,8 @@ const MyOrder = () => {
                                                                 <hr />
                                                             </div> :
 
+                                                            
+
                                                                 <div className='col-12 col-md-6 col-lg-3' >
                                                                     <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
                                                                         <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
